1337ebe87939ef3656986983a22d49e197854eed
Dream0maker <50638086+Dream0maker@users.noreply.github.com>
2021-03-14 14:27:02 +0100

    Add files via upload

--- guiapp.py
+++ guiapp.py
@@ -1,37 +1,7 @@
 from tkinter import ttk
 from tkinter import *
 import pandas as pd
-
-#########################
-# application code      #
-#########################
-# movie lens data set import
-ratings = pd.read_csv('ratings.csv')
-movies = pd.read_csv('movies.csv')
-#
-# # merging the two files to get the movies and ratings in one file + dropping unwanted columns
-# ratings = pd.merge(movies, ratings).drop(['genres', 'timestamp'], axis=1)
-#
-# # changing the data structure to ease the work
-# user_ratings = ratings.pivot_table(index=['userId'], columns=['title'], values='rating')
-# # print(user_ratings.head())
-#
-# # remove movies who were rated by lesser than 10 user and fill Nan with 0
-# user_ratings = user_ratings.dropna(thresh=10, axis=1).fillna(0)
-# # print(user_ratings)
-# # applying the pearson methode to get the similarities between the movies
-# item_similarity_df = user_ratings.corr(method='pearson')
-#
-#
-# # print(item_similarity_df.head(50))
-#
-# def get_similar(movie_name, rating):
-#     # get the similarity score and subtracting 2.5 from the rating to fix placement of bad movies in the list
-#     similar_score = item_similarity_df[movie_name] * (rating - 2.5)
-#     similar_score = similar_score.sort_values(ascending=False)
-#     # print(type(similar_ratings))
-#     return similar_score
-#
+import csv
 
 ###########################
 #           GUI CODE      #
@@ -41,30 +11,53 @@ root.title("Master 2 IAM")
 root.configure(bg='#4b5162')
 root.geometry('800x800')
 
-header = Frame(root, bg='#383c4a')
-content = Frame(root, bg='#4b5162')
-
-# root.columnconfigure(0, weight=1)    # 100%
-# root.rowconfigure(0, weight=1)   # 10%
-# # root.rowconfigure(1, weight=9)   # 80%
-# header.grid(row=0, columnspan=2, sticky=W)
-# content.grid(row=1, sticky='news')
-
-label_header = Label(root, text='Movie Recommender', bg='#383c4a', font="AkayaTelivigala 30 bold").pack()
-# end of header
-moviesL = pd.read_csv('movies.csv',index_col=1).drop(['genres', 'movieId'], axis=1)
+# import the csv file
+filepath = 'movies.csv'
+File = open(filepath)
+Reader = csv.reader(File)
+Data = list(Reader)
+# remove the head "title"
+del(Data[0])
+# append every item parsen into movielist
+movielist = []
+for x in list(range(0, len(Data))):
+    movielist.append(Data[x][1])
+# rating list
 rating = [1, 2, 3, 4, 5]
-# drop down box
-moviecombo = ttk.Combobox(root, value=moviesL).pack(pady=20)
-ratingcombo = ttk.Combobox(root, value=rating).pack()
+# button function to add selected movies
+fake_user=[]
+def addmovie(movie,rating):
+    mov = [movie, rating]
+    fake_user.append(mov)
+    print(fake_user)
 
+# create rappers to manage the gui
+warpper1 = LabelFrame(root, text="Movies Watched")
+warpper2 = LabelFrame(root, text="Movies Suggested")
+warpper1.pack(padx=10, pady=10, fill="both", expand="yes")
+warpper2.pack(padx=10, pady=10, fill="both", expand="yes")
+# drop down box with thier label
+Label(warpper1, text="Select 3 movies you watched and rate them").grid(row=0, column=0, padx=10, pady=10)
+# dropdown menu 1
+moviecombo = ttk.Combobox(warpper1, value=movielist).grid(row=1, column=0, padx=10, pady=10)
+ratingcombo = ttk.Combobox(warpper1, value=rating).grid(row=1, column=2, padx=10, pady=10)
+B1 = Button(warpper1, text="Add Your Movie",
+            command=lambda: addmovie(moviecombo, ratingcombo)).grid(row=1, column=3,padx=10, pady=10)
+# dropdown menu 1
+moviecombo2 = ttk.Combobox(warpper1, value=movielist).grid(row=2, column=0, padx=10, pady=10)
+ratingcombo2 = ttk.Combobox(warpper1, value=rating).grid(row=2, column=2, padx=10, pady=10)
+B2 = Button(warpper1, text="Add Your Movie",
+            command=lambda: addmovie(moviecombo, ratingcombo)).grid(row=2, column=3,padx=10, pady=10)
+# dropdown menu 1
+moviecombo3 = ttk.Combobox(warpper1, value=movielist).grid(row=3, column=0, padx=10, pady=10)
+ratingcombo3 = ttk.Combobox(warpper1, value=rating).grid(row=3, column=2, padx=10, pady=10)
+B3 = Button(warpper1, text="Add Your Movie",
+            command=lambda: addmovie(moviecombo, ratingcombo)).grid(row=3, column=3,padx=10, pady=10)
+# show what user have chosen
+Label(warpper1, text="your selected movies").grid(row=4, column=0, padx=10, pady=10)
+# list result in a list box
+Label(warpper2, text="The Suggested Movies").grid(row=0, column=0, padx=10, pady=10)
 
-# add button and thier function
-def addmovie():
-    print("hello")
-
-
-B = Button(root, text="Add Your Movie", command=addmovie()).pack()
 
 root.mainloop()
 
