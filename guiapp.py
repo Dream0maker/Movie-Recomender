29bd9aa77e65fb96baa6ba7204ab24ef16ec8e71
Dream0maker <50638086+Dream0maker@users.noreply.github.com>
2021-03-15 19:04:51 +0100

    Add files via upload

--- guiapp.py
+++ guiapp.py
@@ -1,35 +1,39 @@
 from tkinter import ttk
 from tkinter import *
 import pandas as pd
-import csv
+
 
 # ############################
 # #          backend         #
 # ############################
 # # movie lens data set import
-# ratings = pd.read_csv('ratings.csv')
-# movies = pd.read_csv('movies.csv')
+ratings = pd.read_csv('ratings.csv')
+movies = pd.read_csv('movies.csv')
 #
 # # merging the two files to get the movies and ratings in one file + dropping unwanted columns
-# ratings = pd.merge(movies, ratings).drop(['genres', 'timestamp'], axis=1)
+ratings = pd.merge(movies, ratings).drop(['genres', 'timestamp'], axis=1)
 #
 # # changing the data structure to ease the work
-# user_ratings = ratings.pivot_table(index=['userId'], columns=['title'], values='rating')
-# # print(user_ratings.head())
+user_ratings = ratings.pivot_table(index=['userId'], columns=['title'], values='rating')
+
 #
 # # remove movies who were rated by lesser than 10 user and fill Nan with 0
-# user_ratings = user_ratings.dropna(thresh=10, axis=1).fillna(0)
-# # print(user_ratings)
+user_ratings = user_ratings.dropna(thresh=10, axis=1).fillna(0)
+
+#movie list shown in dropdown menu
+movie_list=[]
+for i in range(0,len(user_ratings.columns)):
+   movie_list.append(user_ratings.columns[i])
+
+
 # # applying the pearson methode to get the similarities between the movies
-# item_similarity_df = user_ratings.corr(method='pearson')
-# # print(item_similarity_df.head(50))
-#
-# def get_similar(movie_name, rating):
-#     # get the similarity score and subtracting 2.5 from the rating to fix placement of bad movies in the list
-#     similar_score = item_similarity_df[movie_name]*(rating-2.5)
-#     similar_score = similar_score.sort_values(ascending=False)
-#     # print(type(similar_ratings))
-#     return similar_score
+item_similarity_df = user_ratings.corr(method='pearson')
+
+def get_similar(movie_name, rating):
+# # get the similarity score and subtracting 2.5 from the rating to fix placement of bad movies in the list
+  similar_score = item_similarity_df[movie_name]*(rating-2.5)
+  similar_score = similar_score.sort_values(ascending=False)
+  return similar_score
 
 ###########################
 #           GUI CODE      #
@@ -37,46 +41,29 @@ import csv
 root = Tk()
 root.title("Master 2 IAM")
 root.configure(bg='#4b5162')
-root.geometry('700x800')
-
-# import the csv file
-filepath = 'movies.csv'
-File = open(filepath)
-Reader = csv.reader(File)
-Data = list(Reader)
-# remove the head "title"
-del(Data[0])
-
-
-# append every item parsen into movielist
-movielist = []
-for x in list(range(0, len(Data))):
-    movielist.append(Data[x][1])
-# rating list
-rating = [1, 2, 3, 4, 5]
-
+root.geometry('800x900')
 
 # create rappers to manage the gui
 warpper1 = LabelFrame(root, text="Movies Watched")
 warpper2 = LabelFrame(root, text="Movies Suggested")
-warpper1.pack(padx=10, pady=10, fill="both", expand="yes")
-warpper2.pack(padx=10, pady=10, fill="both", expand="yes")
+warpper1.pack(padx=30, pady=40, fill="both", expand="yes")
+warpper2.pack(padx=30, pady=40, fill="both", expand="yes")
 
 
 # drop down box with thier label
 Label(warpper1, text="Select 3 movies you watched and rate them").grid(row=0, column=0, padx=10, pady=10)
 
 # dropdown menu 1
-moviecombo = ttk.Combobox(warpper1, value=movielist)
-ratingcombo = ttk.Combobox(warpper1, value=rating)
+moviecombo = ttk.Combobox(warpper1, value=movie_list)
+ratingcombo = ttk.Combobox(warpper1, value=[1, 2, 3, 4, 5])
 
 # dropdown menu 2
-moviecombo2 = ttk.Combobox(warpper1, value=movielist)
-ratingcombo2 = ttk.Combobox(warpper1, value=rating)
+moviecombo2 = ttk.Combobox(warpper1, value=movie_list)
+ratingcombo2 = ttk.Combobox(warpper1, value=[1, 2, 3, 4, 5])
 
 # dropdown menu 1
-moviecombo3 = ttk.Combobox(warpper1, value=movielist)
-ratingcombo3 = ttk.Combobox(warpper1, value=rating)
+moviecombo3 = ttk.Combobox(warpper1, value=movie_list)
+ratingcombo3 = ttk.Combobox(warpper1, value=[1, 2, 3, 4, 5])
 
 # button function to add selected movies
 fake_user=[]
@@ -122,8 +109,14 @@ def calculate():
     for movie, rating in fake_user:
         similar_movies = similar_movies.append(get_similar(movie, rating), ignore_index=True)
 
-    # printing the recommended list
-    print(similar_movies.sum().sort_values(ascending=False))
+    # printing the top 20 recommended movie
+    s=similar_movies.sum().sort_values(ascending=False)
+    listbox = Listbox(warpper2,)
+    listbox.grid(row=1, column=1, padx=10, pady=10)
+    for i in range(20):
+      listbox.insert(END,s[i:19])
+
+
 
 
 B1 = Button(warpper1, text="Add Your Movie", command=calculate).grid(row=4, column=2, padx=10, pady=10)
@@ -131,9 +124,8 @@ B1 = Button(warpper1, text="Add Your Movie", command=calculate).grid(row=4, colu
 # show what user have chosen
 Label(warpper1, text="your selected movies").grid(row=5, column=0, padx=10, pady=10)
 # list result in a list box
-Label(warpper2, text="The Suggested Movies").grid(row=0, column=0, padx=10, pady=10)
-listbox = Listbox(warpper2)
+Label(warpper2, text="The Suggested Movies").grid(row=0, column=0, padx=50, pady=10)
+listbox = Listbox(warpper2,)
 listbox.grid(row=1, column=1, padx=10, pady=10)
 
 root.mainloop()
-
