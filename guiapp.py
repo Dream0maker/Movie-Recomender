b185fdd8f9aa8008e163e8edc9541cb4181f2099
Dream0maker <50638086+Dream0maker@users.noreply.github.com>
2021-03-15 09:22:46 +0100

    Add files via upload

--- guiapp.py
+++ guiapp.py
@@ -3,33 +3,33 @@ from tkinter import *
 import pandas as pd
 import csv
 
-############################
-#          backend         #
-############################
-# movie lens data set import
-ratings = pd.read_csv('ratings.csv')
-movies = pd.read_csv('movies.csv')
-
-# merging the two files to get the movies and ratings in one file + dropping unwanted columns
-ratings = pd.merge(movies, ratings).drop(['genres', 'timestamp'], axis=1)
-
-# changing the data structure to ease the work
-user_ratings = ratings.pivot_table(index=['userId'], columns=['title'], values='rating')
-# print(user_ratings.head())
-
-# remove movies who were rated by lesser than 10 user and fill Nan with 0
-user_ratings = user_ratings.dropna(thresh=10, axis=1).fillna(0)
-# print(user_ratings)
-# applying the pearson methode to get the similarities between the movies
-item_similarity_df = user_ratings.corr(method='pearson')
-# print(item_similarity_df.head(50))
-
-def get_similar(movie_name, rating):
-    # get the similarity score and subtracting 2.5 from the rating to fix placement of bad movies in the list
-    similar_score = item_similarity_df[movie_name]*(rating-2.5)
-    similar_score = similar_score.sort_values(ascending=False)
-    # print(type(similar_ratings))
-    return similar_score
+# ############################
+# #          backend         #
+# ############################
+# # movie lens data set import
+# ratings = pd.read_csv('ratings.csv')
+# movies = pd.read_csv('movies.csv')
+#
+# # merging the two files to get the movies and ratings in one file + dropping unwanted columns
+# ratings = pd.merge(movies, ratings).drop(['genres', 'timestamp'], axis=1)
+#
+# # changing the data structure to ease the work
+# user_ratings = ratings.pivot_table(index=['userId'], columns=['title'], values='rating')
+# # print(user_ratings.head())
+#
+# # remove movies who were rated by lesser than 10 user and fill Nan with 0
+# user_ratings = user_ratings.dropna(thresh=10, axis=1).fillna(0)
+# # print(user_ratings)
+# # applying the pearson methode to get the similarities between the movies
+# item_similarity_df = user_ratings.corr(method='pearson')
+# # print(item_similarity_df.head(50))
+#
+# def get_similar(movie_name, rating):
+#     # get the similarity score and subtracting 2.5 from the rating to fix placement of bad movies in the list
+#     similar_score = item_similarity_df[movie_name]*(rating-2.5)
+#     similar_score = similar_score.sort_values(ascending=False)
+#     # print(type(similar_ratings))
+#     return similar_score
 
 ###########################
 #           GUI CODE      #
@@ -37,7 +37,7 @@ def get_similar(movie_name, rating):
 root = Tk()
 root.title("Master 2 IAM")
 root.configure(bg='#4b5162')
-root.geometry('800x800')
+root.geometry('700x800')
 
 # import the csv file
 filepath = 'movies.csv'
@@ -81,38 +81,38 @@ ratingcombo3 = ttk.Combobox(warpper1, value=rating)
 # button function to add selected movies
 fake_user=[]
 def addmovie(event):
-        Label(warpper1, text=ratingcombo.get())
-        mov = [moviecombo.get(), ratingcombo.get()]
+        Label(warpper1, text=moviecombo.get()).grid(row=6, column=0, padx=10, pady=10)
+        mov = [moviecombo.get(), int(ratingcombo.get())]
         fake_user.append(mov)
         print(fake_user)
 
 
 def addmovie2(event):
-        Label(warpper1, text=ratingcombo.get())
-        mov = [moviecombo2.get(), ratingcombo2.get()]
+        Label(warpper1, text=moviecombo2.get()).grid(row=6, column=2, padx=10, pady=10)
+        mov = [moviecombo2.get(), int(ratingcombo2.get())]
         fake_user.append(mov)
         print(fake_user)
 
 
 def addmovie3(event):
-        Label(warpper1, text=ratingcombo.get())
-        mov = [moviecombo3.get(), ratingcombo3.get()]
+        Label(warpper1, text=moviecombo3.get()).grid(row=6, column=3, padx=10, pady=10)
+        mov = [moviecombo3.get(), int(ratingcombo3.get())]
         fake_user.append(mov)
         print(fake_user)
 
 # comboSelect bind 1
 ratingcombo.current(0)
 ratingcombo.bind("<<ComboboxSelected>>", addmovie)
-ratingcombo.grid(row=1, column=2, padx=10, pady=10)
+ratingcombo.grid(row=1, column=3, padx=10, pady=10)
 moviecombo.current(0)
 moviecombo.grid(row=1, column=0, padx=10, pady=10)
 # comboSelect bind 2
 ratingcombo2.bind("<<ComboboxSelected>>", addmovie2)
-ratingcombo2.grid(row=2, column=2, padx=10, pady=10)
+ratingcombo2.grid(row=2, column=3, padx=10, pady=10)
 moviecombo2.grid(row=2, column=0, padx=10, pady=10)
 # comboSelect bind 3
 ratingcombo3.bind("<<ComboboxSelected>>", addmovie3)
-ratingcombo3.grid(row=3, column=2, padx=10, pady=10)
+ratingcombo3.grid(row=3, column=3, padx=10, pady=10)
 moviecombo3.grid(row=3, column=0, padx=10, pady=10)
 
 
@@ -126,13 +126,14 @@ def calculate():
     print(similar_movies.sum().sort_values(ascending=False))
 
 
-B1 = Button(warpper1, text="Add Your Movie", command=calculate).grid(row=3, column=3, padx=10, pady=10)
+B1 = Button(warpper1, text="Add Your Movie", command=calculate).grid(row=4, column=2, padx=10, pady=10)
 
 # show what user have chosen
-Label(warpper1, text="your selected movies").grid(row=4, column=0, padx=10, pady=10)
+Label(warpper1, text="your selected movies").grid(row=5, column=0, padx=10, pady=10)
 # list result in a list box
 Label(warpper2, text="The Suggested Movies").grid(row=0, column=0, padx=10, pady=10)
-
+listbox = Listbox(warpper2)
+listbox.grid(row=1, column=1, padx=10, pady=10)
 
 root.mainloop()
 
