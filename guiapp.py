1bcd9c619c6b670f756d7956f7f72eecce6f0aca
Dream0maker <50638086+Dream0maker@users.noreply.github.com>
2021-03-15 09:02:19 +0100

    Add files via upload

--- guiapp.py
+++ guiapp.py
@@ -3,6 +3,34 @@ from tkinter import *
 import pandas as pd
 import csv
 
+############################
+#          backend         #
+############################
+# movie lens data set import
+ratings = pd.read_csv('ratings.csv')
+movies = pd.read_csv('movies.csv')
+
+# merging the two files to get the movies and ratings in one file + dropping unwanted columns
+ratings = pd.merge(movies, ratings).drop(['genres', 'timestamp'], axis=1)
+
+# changing the data structure to ease the work
+user_ratings = ratings.pivot_table(index=['userId'], columns=['title'], values='rating')
+# print(user_ratings.head())
+
+# remove movies who were rated by lesser than 10 user and fill Nan with 0
+user_ratings = user_ratings.dropna(thresh=10, axis=1).fillna(0)
+# print(user_ratings)
+# applying the pearson methode to get the similarities between the movies
+item_similarity_df = user_ratings.corr(method='pearson')
+# print(item_similarity_df.head(50))
+
+def get_similar(movie_name, rating):
+    # get the similarity score and subtracting 2.5 from the rating to fix placement of bad movies in the list
+    similar_score = item_similarity_df[movie_name]*(rating-2.5)
+    similar_score = similar_score.sort_values(ascending=False)
+    # print(type(similar_ratings))
+    return similar_score
+
 ###########################
 #           GUI CODE      #
 ###########################
@@ -86,7 +114,19 @@ moviecombo2.grid(row=2, column=0, padx=10, pady=10)
 ratingcombo3.bind("<<ComboboxSelected>>", addmovie3)
 ratingcombo3.grid(row=3, column=2, padx=10, pady=10)
 moviecombo3.grid(row=3, column=0, padx=10, pady=10)
-# B1 = Button(warpper1, text="Add Your Movie", command=lambda: addmovie(moviecombo, ratingcombo)).grid(row=1, column=3,padx=10, pady=10)
+
+
+def calculate():
+    # collecting similar movies so we can show the result
+    similar_movies = pd.DataFrame()
+    for movie, rating in fake_user:
+        similar_movies = similar_movies.append(get_similar(movie, rating), ignore_index=True)
+
+    # printing the recommended list
+    print(similar_movies.sum().sort_values(ascending=False))
+
+
+B1 = Button(warpper1, text="Add Your Movie", command=calculate).grid(row=3, column=3, padx=10, pady=10)
 
 # show what user have chosen
 Label(warpper1, text="your selected movies").grid(row=4, column=0, padx=10, pady=10)
