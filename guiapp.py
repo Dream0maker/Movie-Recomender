bc1cb36f4f50dc12ac6d40b9843ad6c99f4b002d
Dream0maker <50638086+Dream0maker@users.noreply.github.com>
2021-03-29 07:39:26 -0100

    Update guiapp.py

--- guiapp.py
+++ guiapp.py
@@ -34,31 +34,6 @@ def get_similar(movie_name, rating):
     return similar_score
 
 
-def checkkey(event):
-    value = event.widget.get()
-
-    # get data from movie list
-    if value == '':
-        data = movie_list
-    else:
-        data = []
-        for item in movie_list:
-            if value.lower() in item.lower():
-                data.append(item)
-
-                # update data in listbox
-    update(data)
-
-
-def update(data):
-    # clear previous data
-    lb.delete(0, 'end')
-
-    # put new data
-    for item in data:
-        lb.insert('end', item)
-
-
 ################################################################################################################
 #                                                  GUI CODE                                                    #
 ################################################################################################################
@@ -66,7 +41,7 @@ def update(data):
 root = Tk()
 root.title("Master 2 IAM")
 root.configure(bg='#4b5162')
-root.geometry('700x800')
+root.geometry('800x700')
 root.resizable(False, False)
 
 # create rappers to manage the gui
@@ -74,73 +49,77 @@ wrapper1 = Frame(root, bg='#383c4a')
 wrapper2 = Frame(root, bg='#4b5162')
 wrapper3 = Frame(root, bg='#4b5162')
 wrapper1.pack(padx=0, pady=0, fill="both")
-wrapper2.pack(padx=30, pady=10, fill="both", expand="yes")
-wrapper3.pack(padx=30, pady=10, fill="both", expand="yes")
+wrapper2.pack(padx=30, pady=40, fill="both", expand="yes")
+wrapper3.pack(padx=30, pady=40, fill="both", expand="yes")
 
-#########################
-#         header        #
-#########################
+# header
+
 Label(wrapper1, text="MOVIE RECOMMENDER", bg='#383c4a', fg='white', font=("Chango", 33)).pack(pady=10)
-
-#########################
-#     main content      #
-#########################
 # drop down box with their label
-Label(wrapper2, text="Select at least ONE movie", fg='white', bg='#4b5162', font=("Chango", 10)).grid(
-    row=0, column=0, padx=10, pady=10)
+Label(wrapper2, text="Select 3 movies you watched and rate them", fg='white', bg='#4b5162', font=("Chango", 10)).grid(row=0, column=0, padx=10, pady=10)
 # dropdown menu 1
-movie_combo = Entry(wrapper2, width=63)
+movie_combo = ttk.Combobox(wrapper2, value=movie_list, width=60)
 rating_combo = ttk.Combobox(wrapper2, value=[0, 1, 2, 3, 4, 5], width=32)
-# creating list box
-lb = Listbox(wrapper2, width=95)
+# dropdown menu 2
+movie_combo2 = ttk.Combobox(wrapper2, value=movie_list, width=60)
+rating_combo2 = ttk.Combobox(wrapper2, value=[0, 1, 2, 3, 4, 5], width=32)
+# dropdown menu 1
+movie_combo3 = ttk.Combobox(wrapper2, value=movie_list, width=60)
+rating_combo3 = ttk.Combobox(wrapper2, value=[0, 1, 2, 3, 4, 5], width=32)
 
-# grid element to the screen
-rating_combo.grid(row=1, column=3, padx=10, pady=10)
-movie_combo.grid(row=1, column=0, columnspan=3, padx=10, pady=10)
-lb.grid(row=3, column=0, columnspan=4, padx=10, pady=10)
-
-# call for the keystrock function
-update(movie_list)
 # button function to add selected movies
 fake_user = []
 
 
 def add_movie(event):
+    # e1.insert(END, movie_combo.get())
     mov = [movie_combo.get(), int(rating_combo.get())]
     fake_user.append(mov)
-    global labeltest
-    labeltest = Label(wrapper2, text=fake_user)
-    labeltest.grid(row=4, column=0,columnspan=4 , padx=10, pady=10)
 
 
-def fill(event):
-    movie_combo.delete(0, END)
-    movie_combo.insert(0, lb.get(ACTIVE))
+def add_movie2(event):
+    # e2.insert(END, movie_combo2.get())
+    mov = [movie_combo2.get(), int(rating_combo2.get())]
+    fake_user.append(mov)
 
 
-# comboSelect bind
+def add_movie3(event):
+    # e3.insert(END, movie_combo3.get())
+    mov = [movie_combo3.get(), int(rating_combo3.get())]
+    fake_user.append(mov)
+
+
+# comboSelect bind 1
 rating_combo.bind("<<ComboboxSelected>>", add_movie)
-# key release bind
-movie_combo.bind('<KeyRelease>', checkkey)
-# list box bind to print selected item in entry box
-lb.bind("<<ListboxSelect>>", fill)
-
-# label for wrapper 3
+rating_combo.grid(row=1, column=3, padx=10, pady=10)
+movie_combo.grid(row=1, column=0, columnspan=3, padx=10, pady=10)
+# comboSelect bind 2
+rating_combo2.bind("<<ComboboxSelected>>", add_movie2)
+rating_combo2.grid(row=3, column=3, padx=10, pady=10)
+movie_combo2.grid(row=3, column=0,  columnspan=3, padx=10, pady=10)
+# comboSelect bind 3
+rating_combo3.bind("<<ComboboxSelected>>", add_movie3)
+rating_combo3.grid(row=5, column=3, padx=10, pady=10)
+movie_combo3.grid(row=5, column=0,  columnspan=3, padx=10, pady=10)
+# list result in a list box
 Label(wrapper3, text="Our Recommendation", bg='#4b5162', fg='white', font=("Chango", 10)).pack(pady=20)
 # show the movie recommended with their ratings
-tree = ttk.Treeview(wrapper3)  # Treeview instead of listbox
+# result = Listbox(wrapper3, width=200)
+# result.pack()
+# Treeview instead of listbox
+tree = ttk.Treeview(wrapper3)
 # define columns
 tree['columns'] = ("ID", "Movie Name")
 # format Treeview
 tree.column("#0", width=0, stretch=NO)
-tree.column("ID", anchor=W, width=25)
-tree.column("Movie Name", width=600, anchor=W)
+tree.column("ID", anchor=W, width=20)
+tree.column("Movie Name", width=650, anchor=W)
 # headings
 tree.heading("#0", text="", anchor=W)
 tree.heading("ID", text="#", anchor=W)
 tree.heading("Movie Name", text="Movie Name", anchor=W)
 # pack
-tree.pack(padx=10, pady=10)
+tree.pack()
 # # create error message when movies not selected
 error_message = StringVar()
 
@@ -148,7 +127,7 @@ error_message = StringVar()
 # button to suggest movie
 def calculate():
     # collecting similar movies so we can show the result
-    if not any(movie_combo.get()):
+    if not any((movie_combo.get(), movie_combo2.get(), movie_combo3.get())):
         error_message = ['please enter at least one movie !']
         tree.insert(parent='', index='end', iid=0, text='', values=(1, error_message))
     else:
@@ -171,19 +150,18 @@ def clear():
     fake_user.clear()
     movie_combo.delete(0, END)
     rating_combo.delete(0, END)
-    lb.delete(0, END)
-    for item in movie_list:
-        lb.insert(END, item)
+    movie_combo2.delete(0, END)
+    rating_combo2.delete(0, END)
+    movie_combo3.delete(0, END)
+    rating_combo3.delete(0, END)
     for record in tree.get_children():
         tree.delete(record)
-    labeltest.destroy()
 
 
 # create buttons
-B1 = Button(wrapper2, text="Get Recommendation", bg='#7c818c', fg='white', padx=10, pady=10, borderwidth=2,
-            command=calculate).grid(row=8, column=0, padx=20, pady=10)
-B2 = Button(wrapper2, text="     clear    ", bg='#7c818c', fg='white', padx=24, pady=10, borderwidth=2,
-            command=clear).grid(row=8, column=3)
+B1 = Button(wrapper2, text="Get Recommendation", bg='#7c818c', fg='white', padx=10, pady=10, borderwidth=2, command=calculate).grid(row=8, column=0, padx=20, pady=10)
+B2 = Button(wrapper2, text="     clear    ", bg='#7c818c', fg='white', padx=24, pady=10, borderwidth=2, command=clear).grid(row=8, column=3)
+
 
 root.resizable(width=False, height=True)
 root.mainloop()
